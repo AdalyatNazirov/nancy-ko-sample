4b146f1c99cd320f3d29d3e80f6a3864c53bd39c
Adalyat Nazirov <aenazirov@gmail.com>
2014-01-25 14:51:01 +0400

    added partials for side block elements

--- NancyWebBlog.Model/Context/Entities/Post.cs
+++ NancyWebBlog.Model/Context/Entities/Post.cs
@@ -9,6 +9,8 @@ namespace NancyWebBlog.Model
     {
         public int ID { get; set; }
 
+        public string Title { get; set; }
+
         public string PreBody { get; set; }
         
         public string Body { get; set; }
