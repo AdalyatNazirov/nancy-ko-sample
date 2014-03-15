e8a3431c6307f4e28be1ddeaaa304ea8b03a9927
Adalyat Nazirov <aenazirov@gmail.com>
2014-03-15 22:08:45 +0400

    post moved to html template.

--- NancyWebBlog/CassetteConfiguration.cs
+++ NancyWebBlog/CassetteConfiguration.cs
@@ -1,4 +1,5 @@
 using Cassette;
+using Cassette.HtmlTemplates;
 using Cassette.Scripts;
 using Cassette.Stylesheets;
 using System.Text.RegularExpressions;
@@ -16,13 +17,19 @@ namespace NancyWebBlog
             //reference in release mode http://getcassette.net/documentation/v1/AssetReferences
             //maybe external scripts http://getcassette.net/documentation/v1/scripts/external
             AddStylesheetBundles(bundles);
+            AddTemplateBundles(bundles);
             AddScriptBundles(bundles);
 
         }
 
         private static void AddStylesheetBundles(BundleCollection bundles)
         {
-            bundles.AddPerSubDirectory<StylesheetBundle>("~/Content");
+            bundles.AddPerSubDirectory<StylesheetBundle>("~/Content/css");
+        }
+
+        private static void AddTemplateBundles(BundleCollection bundles)
+        {
+            bundles.AddPerSubDirectory<HtmlTemplateBundle>("~/Content/templates");
         }
 
         private static void AddScriptBundles(BundleCollection bundles)
